f6b318c5f82cfc359752d10088146e51421ec616
Pedro Nascimento <pnascimento@gmail.com>
2011-04-04 05:55:38 -0300

    twitter

---
+++ public/js/twitter.js
@@ -0,0 +1,38 @@
+		function twitterReactions (json) {
+			// Checking the JSON response
+			var results = json.response.list;
+				
+			// If there are any results, iterate over them			
+			if (typeof results !== "undefined") {
+				var resultsLength = results.length,
+					twitterReactionsPresenter = document.getElementById("twitter-reactions"),
+					twitterReactionsHeader = document.createElement("h3"),
+					tweets = '<ul class="comments">',
+					twitterReactionsText = resultsLength + " Twitter reaction" + ((resultsLength > 1)? "s" : ""),
+					current,
+					currentAuthor,
+					tweet;
+ 
+				// Creating header before tweets container
+				twitterReactionsHeader.id = "twitter-reactions-header";
+				twitterReactionsHeader.innerHTML = twitterReactionsText;
+				twitterReactionsPresenter.parentNode.insertBefore(twitterReactionsHeader, twitterReactionsPresenter);
+ 
+				// Iterating over all tweets
+				for (var i=resultsLength-1; i>=0; i--) {
+					current = results[i];
+					currentAuthor = current.author;
+					tweet = '<li>';
+					tweet += '<img src="' + currentAuthor.photo_url + '" alt="" width="48" height="48" alt="">';
+					tweet += '<a href="' + currentAuthor.url + '">' + currentAuthor.nick + " (" + currentAuthor.name + ")" + "</a><br>";
+					tweet += current.date_alpha;
+					tweet += '<div class="comment-text">' + current.content.replace(/(http:\/\/[\w\.\d%\/\-\_]+)/gi, '<a href="$1">$1</a>') + '</div>';
+					tweet += '</li>';
+					tweets += tweet;
+				}
+				tweets += '</ul>';
+				
+				// Apply all tweets into the tweet presenting element
+				twitterReactionsPresenter.innerHTML = tweets;
+			}
+		}
